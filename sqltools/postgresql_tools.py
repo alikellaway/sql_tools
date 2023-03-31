587144f91235c5f31d61d6c77bbfcd56fb46e4b4
Ali Kellaway <alistiar.kellaway@deltacapita.com>
2023-03-31 10:26:08 +0100

    Added create table and optimisations.

--- sqltools/postgresql_tools.py
+++ sqltools/postgresql_tools.py
@@ -8,19 +8,15 @@ def insertion(table_name: str, names_values: dict[str:str] | list[dict[str:str]]
     :return: A string insert statement able to insert the given information.
     """
     outstr = f'INSERT INTO {table_name}\n'
-    args_serialise = lambda value_dict: " ".join(map(lambda v: f'{value_writer(v)},', value_dict.values()))[:-1]
+    args_serialise = lambda value_dict: ", ".join(map(lambda v: f'{value_writer(v)}', value_dict.values()))
     if isinstance(names_values, list):
         names = names_values[0].keys()
-        values_str = ""
-        for d in names_values:
-            values_str += f'({args_serialise(d)}),\n'
+        values_str = ", \n".join(map(lambda d: f'({args_serialise(d)})', names_values))
     elif isinstance(names_values, dict):
         names = names_values.keys()
-        values_str = f'({args_serialise(names_values)}),\n'
+        values_str = f'({args_serialise(names_values)})'
     names_str = " ".join(map(lambda n: f'{n},', names))
-    outstr += f'({names_str[:-1]})\nVALUES\n'
-    outstr += values_str[:-2] + ";"
-    return outstr
+    return outstr + f'({names_str[:-1]})\nVALUES\n{values_str};'
 
 
 def update(table_name: str, names_values: dict[str:str], where: str = None) -> str:
@@ -32,19 +28,26 @@ def update(table_name: str, names_values: dict[str:str], where: str = None) -> s
     :return: A string that can be used to update the table <table_name> with the values given in a postgre db.
     """
     outstr = f'UPDATE {table_name}\nSET '
-    outstr += "\n    ".join(map(lambda kvp: f'{kvp[0]} = {value_writer(kvp[1])},', names_values.items()))[:-1]
+    outstr += ",\n    ".join(map(lambda kvp: f'{kvp[0]} = {value_writer(kvp[1])}', names_values.items()))
     if where is not None:
         outstr += f'\nWHERE {where}'
     return outstr + ";"
 
 
+def create_table(table_name: str, names_types: dict[str:str]) -> str:
+    outstr = f'CREATE TABLE {table_name} ('
+    outstr += ",\n\t".join(map(lambda kvp: f'{kvp[0]} {kvp[1]}', names_types.items()))
+    return outstr + "\n);"
+
 
 if __name__ == '__main__':
-
     d = {
         "contact_id": "id1",
         "name": "name1",
         "sname": "sname1"
     }
     dl = [d, d, d, d]
-    print(update("table1", d))
+    # print(update("table1", d))
+    print(insertion("table2", d))
+
+    # print(create_table("table1", d))
