b56f3f2b4433fe18c672f2667c1f8e86e6b61812
Ali Kellaway <alistiar.kellaway@deltacapita.com>
2023-03-31 09:01:42 +0100

    Value handler functions into their own module.

--- sqltools/sql_tools.py
+++ sqltools/sql_tools.py
@@ -2,6 +2,7 @@ from typing import Any
 from pathlib import Path
 import csv
 import logging
+from value_handling import value_reader, value_writer
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -52,48 +53,6 @@ def update(table_name: str, names_values: dict[str:Any], where: None | Any = Non
     # keyvaluepair
     outstr = outstr[:-1] + (";" if where is None else f'\nWHERE {where};')
     return outstr
-
-
-def value_writer(value: Any) -> str:
-    """
-    Takes an object and converts it into a string which a SQL compiler could interpret.
-    :param value: A Python object which is to be converted into a string.
-    :return: A string representation of an object which can be understood by a SQL compiler.
-    """
-    if isinstance(value, str):
-        return f'"{value}"'
-    if isinstance(value, int) or isinstance(value, float):
-        return f'{value}'
-    if value is None:
-        return f'NULL'
-    raise NotImplemented("Unable to handle input of type: " + str(type(value)))
-
-
-def value_reader(value: str) -> Any:
-    """
-    Aims to read strings from csv files and convert them into Python object for correct operation.
-    :param value: The string to attempt to convert.
-    :return: A python object representing the input value, if possible and supported.
-    """
-    if not isinstance(value, str):
-        return value
-    # If some null entity.
-    if value.capitalize() in ["NONE", "NULL"]:
-        return "NULL"
-    else:
-        logger.debug(f'Failed to recognize {value} as a null entity.')
-    # Try int conversion
-    try:
-        return int(value)
-    except ValueError:
-        logger.debug(f'Failed to convert {value} to int.')
-    # Try float conversion
-    try:
-        return float(value)
-    except ValueError:
-        logger.debug(f'Failed to convert {value} to float.')
-
-    return value
 
 
 def csv_to_inserts(path: str | Path, table_name: str) -> str:
---
+++ sqltools/value_handling.py
@@ -0,0 +1,47 @@
+import logging
+from typing import Any
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+def value_writer(value: Any) -> str:
+    """
+    Takes an object and converts it into a string which a SQL compiler could interpret.
+    :param value: A Python object which is to be converted into a string.
+    :return: A string representation of an object which can be understood by a SQL compiler.
+    """
+    if isinstance(value, str):
+        return f'"{value}"'
+    if isinstance(value, int) or isinstance(value, float):
+        return f'{value}'
+    if value is None:
+        return f'NULL'
+    raise NotImplemented("Unable to handle input of type: " + str(type(value)))
+
+
+def value_reader(value: str) -> Any:
+    """
+    Aims to read strings from csv files and convert them into Python object for correct operation.
+    :param value: The string to attempt to convert.
+    :return: A python object representing the input value, if possible and supported.
+    """
+    if not isinstance(value, str):
+        return value
+    # If some null entity.
+    if value.capitalize() in ["NONE", "NULL"]:
+        return "NULL"
+    else:
+        logger.debug(f'Failed to recognize {value} as a null entity.')
+    # Try int conversion
+    try:
+        return int(value)
+    except ValueError:
+        logger.debug(f'Failed to convert {value} to int.')
+    # Try float conversion
+    try:
+        return float(value)
+    except ValueError:
+        logger.debug(f'Failed to convert {value} to float.')
+
+    return value
