5f2d052b8d4d68b69c889ac7dac8874820568ed9
Ali Kellaway <alistiar.kellaway@deltacapita.com>
2023-03-31 09:03:23 +0100

    Removed the value funcs

--- sqltools/sql_tools.py
+++ sqltools/sql_tools.py
@@ -78,48 +78,6 @@ def update(table_name: str, names_values: dict[str:Any], where: None | Any = Non
     return outstr
 
 
-def value_writer(value: Any) -> str:
-    """
-    Takes an object and converts it into a string which a SQL compiler could interpret.
-    :param value: A Python object which is to be converted into a string.
-    :return: A string representation of an object which can be understood by a SQL compiler.
-    """
-    if isinstance(value, str):
-        return f'"{value}"'
-    if isinstance(value, int) or isinstance(value, float):
-        return f'{value}'
-    if value is None:
-        return f'NULL'
-    raise NotImplemented("Unable to handle input of type: " + str(type(value)))
-
-
-def value_reader(value: str) -> Any:
-    """
-    Aims to read strings from csv files and convert them into Python object for correct operation.
-    :param value: The string to attempt to convert.
-    :return: A python object representing the input value, if possible and supported.
-    """
-    if not isinstance(value, str):
-        return value
-    # If some null entity.
-    if value.capitalize() in ["NONE", "NULL"]:
-        return "NULL"
-    else:
-        logger.debug(f'Failed to recognize {value} as a null entity.')
-    # Try int conversion
-    try:
-        return int(value)
-    except ValueError:
-        logger.debug(f'Failed to convert {value} to int.')
-    # Try float conversion
-    try:
-        return float(value)
-    except ValueError:
-        logger.debug(f'Failed to convert {value} to float.')
-
-    return value
-
-
 def csv_to_inserts(path: str | Path, table_name: str) -> str:
     """
     Converts a .csv file into a list of Insert statement strings that can be executed in a SQL Database.
@@ -153,7 +111,7 @@ if __name__ == '__main__':
         "k3": None,
         "k4": "v4"
     }
-    
+
     t = {
         "PersonID": "int",
         "LastName": "varchar(255)",
@@ -161,7 +119,6 @@ if __name__ == '__main__':
         "Address": "varchar(255)",
         "City": "varchar(255)"
     }
-    
 
     # print(insertion("table", d))
     # print(proc_call("proc1", d))
