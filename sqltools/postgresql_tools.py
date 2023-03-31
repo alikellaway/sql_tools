c0efe62577c7c397401dea4b8fe4fc5bf434a53d
Ali Kellaway <alistiar.kellaway@deltacapita.com>
2023-03-31 09:16:02 +0100

    postgre insert done

--- sqltools/postgresql_tools.py
+++ sqltools/postgresql_tools.py
@@ -1,26 +1,28 @@
+from value_handling import value_reader, value_writer
 
 def insertion(table_name: str, names_values: dict[str:str] | list[dict[str:str]]) -> str:
     outstr = f'INSERT INTO {table_name}\n'
+    args_serialise = lambda value_dict: " ".join(map(lambda v: f'{value_writer(v)},', value_dict.values()))[:-1]
     if isinstance(names_values, list):
         names = names_values[0].keys()
         values_str = ""
         for d in names_values:
-            d_str = " ".join(map(lambda v: f'{v},', d.values()))
-
-            values += f'()'
-    else:
+            values_str += f'({args_serialise(d)}),\n'
+    elif isinstance(names_values, dict):
         names = names_values.keys()
-        values_str = f''
+        values_str = f'({args_serialise(names_values)}),\n'
     names_str = " ".join(map(lambda n: f'{n},', names))
     outstr += f'({names_str[:-1]})\nVALUES\n'
-    outstr += values_str
+    outstr += values_str[:-2] + ";"
     return outstr
 
 
 if __name__ == '__main__':
+
     d = {
         "contact_id": "id1",
         "name": "name1",
         "sname": "sname1"
     }
+    dl = [d, d, d, d]
     print(insertion("table1", d))
