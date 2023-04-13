112c909b77d93cceeb51b7af7dc7140316db112c
Ali Kellaway <alistiar.kellaway@deltacapita.com>
2023-04-13 13:49:29 +0100

    Added documentationary comments to the existing functions.

--- sqltools/postgre.py
+++ sqltools/postgre.py
@@ -1,5 +1,6 @@
 from value_handling import value_writer
 import logging
+from typing import Any
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -45,6 +46,11 @@ def update(table_name: str, names_values: dict[str:str], where: str = None) -> s
 
 
 def create_table(table_name: str, names_types: dict[str:str]) -> str:
+    """
+    Generates a string that can be used to create a table of the given name and types when the string is executed.
+    :param table_name: The name of the table to create.
+    :param names_types: A dictionary mapping the field names to the field types.
+    """
     outstr = f'CREATE TABLE {table_name} ('
     outstr += ",\n\t".join(
         map(lambda kvp: f'{kvp[0]} {kvp[1]}', names_types.items())) + "\n);"
@@ -52,13 +58,25 @@ def create_table(table_name: str, names_types: dict[str:str]) -> str:
     return outstr
 
 
-def procedure_call(proc_name: str, param_args: dict[str:str]) -> str:
+def procedure_call(proc_name: str, param_args: dict[str:Any]) -> str:
+    """
+    Generates a string that can be used to call a procedure of a given name with the given arguments when executed.
+    :param proc_name: The name of the procedure to call.
+    :param param_args: The names of the parameters mapped to their respective values.
+    """
     outstr = f'CALL {proc_name}({", ".join(map(lambda kvp: value_writer(kvp[1]), param_args.items()))});'
     logging.debug(f'Created {__name__} proceduce call:\n{outstr}')
     return outstr
 
 
 def drop_table(table_names: str | list, if_exists: bool = False, cascade: bool = False, restrict: bool = False):
+    """
+    Generates a string that can be used to drop a table given its name (or multiple tables when given a list of names) when executed.
+    :param table_names: The name or list of names of table(s) to drop. NOTE: You cannot add cascade and restrict to the same string.
+    :param if_exists: Whether to add if exists to the string.
+    :param cascade: Whether to add CASCADE to the string.
+    :param restrict: Whether to add RESTRICT to the string.
+    """
     outstr = f'DROP TABLE '
     outstr += " IF EXISTS  " if if_exists else ""
     outstr += table_names if isinstance(table_names, str) else ", ".join(table_names)
@@ -78,4 +96,4 @@ if __name__ == '__main__':
     # print(insertion("table2", d))
     # print(procedure_call("proc_name", d))
     # print(create_table("table1", d))
-    print(drop_table(["table", "table2"]))
+    print(drop_table(["table", "table2", "table3"]))
