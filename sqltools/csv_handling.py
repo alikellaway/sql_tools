3fc4c5ad7755cef9500451b6b1d19f32869fc226
Ali Kellaway <alistiar.kellaway@deltacapita.com>
2023-04-13 18:28:00 +0100

    CSV reader optimisations and betterment.

--- sqltools/csv_handling.py
+++ sqltools/csv_handling.py
@@ -9,21 +9,24 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-class CSVFileWrapper:
+class CSVFile:
     def __init__(self, path: str | Path):
         self.path = handle_path(path)
         self.reader = None
         self.header = None
+        self.rows = None
 
     def __enter__(self):
         self.csv = open(self.path, 'rt')
-        self.csvr = reader(self.csv)
-        self.header = self.csvr.__next__
+        self.rows = reader(self.csv)
+        self.header = self.rows.__next__()
+        return self
 
-    def __exit__(self):
+    def __exit__(self, exc_type, exc_value, traceback):
         self.csv.close()
         self.reader = None
         self.header = None
+        self.rows = None
 
 
 def handle_path(path: str | Path):
@@ -35,8 +38,8 @@ def handle_path(path: str | Path):
         raise NotImplementedError()
     return path
 
-
-def csv_to_inserts(path: str | Path, table_name: str, insertion_func: Callable = insertion) -> str:
+    
+def csv_to_inserts(path: str | Path, table_name: str, insertion_func: Callable = insertion):
     """
     Converts a .csv file into a list of insert statement strings that can be executed in a SQL Database.
     :param path: The path of the file to convert.
@@ -44,19 +47,18 @@ def csv_to_inserts(path: str | Path, table_name: str, insertion_func: Callable =
     :param insertion_func: The function to use to generate the insertion statements i.e. from postgre, or sql?
     :return: A list of insert strings.
     """
-    path = handle_path(path)
-    with open(path, 'rt') as file:
-        csvreader = reader(file)
-        fields = csvreader.__next__()
-        insert_dicts = []
-        for row in csvreader:
+    with CSVFile(path) as f:
+        insert_strings = []
+        for row in f.rows:
+            # Create a with values to convert enable insertion convertion
             row_dict = {}
             for idx, value in enumerate(row):
-                row_dict[fields[idx]] = value_reader(value.lstrip().strip())
-            insert_dicts.append(row_dict)
-
-        return list(map(lambda idict: insertion_func(table_name, idict), insert_dicts))
+                row_dict[f.header[idx]] = value_reader(value.lstrip().rstrip())
+            insert_strings.append(insertion_func(table_name, row_dict))
+    return insert_strings
+            
+    
   
 
 # def csv_to_updates(path: str | Path, table_name: str, update_func: Callable = update):
-print((handle_path("C:/Users/AlistairKellaway/Downloads/snakes_count_100.csv")))
+print((csv_to_inserts2("C:/Users/AlistairKellaway/Downloads/snakes_count_100.csv", table_name="name")))
