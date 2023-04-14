ab3d9bc341cbd14a17d273d75b5d1d1e46c875a9
Ali Kellaway <alistiar.kellaway@deltacapita.com>
2023-04-14 10:26:53 +0100

    Added csv to updates and added docu comments.

--- sqltools/csv_handling.py
+++ sqltools/csv_handling.py
@@ -1,5 +1,5 @@
 from value_handling import value_reader
-from typing import Callable
+from typing import Callable, Any
 from pathlib import Path
 from csv import reader
 from transact import insertion, update
@@ -10,7 +10,15 @@ logger.setLevel(logging.INFO)
 
 
 class CSVFile:
+    """
+    Class abstracts CSVFile loading to make iterating through rows easier.
+    """
+
     def __init__(self, path: str | Path):
+        """
+        Constructs a new CSVFile object given the path of the csv file to load.
+        :param path: The path of the csv file to load.
+        """
         self.path = handle_path(path)
         self.reader = None
         self.header = None
@@ -38,7 +46,21 @@ def handle_path(path: str | Path):
         raise NotImplementedError()
     return path
 
-    
+
+def __row_to_header_row_dict(header: list[str], row: list[str]) -> dict[str:Any]:
+    """
+    Geven a csv files header and a row from that file, this function will output a dictionary 
+    mapping each header title to its respective value.
+    :param header: The header of the csv file.
+    :param row: The row of the csv file.
+    :return: row_dict The dictionary representing that row.
+    """
+    row_dict = {}
+    for idx, value in enumerate(row):
+        row_dict[header[idx]] = value_reader(value.lstrip().rstrip())
+    return row_dict
+
+
 def csv_as_inserts(path: str | Path, table_name: str, insertion_func: Callable = insertion):
     """
     Converts a .csv file into a list of insert statement strings that can be executed in a SQL Database.
@@ -50,13 +72,26 @@ def csv_as_inserts(path: str | Path, table_name: str, insertion_func: Callable =
     with CSVFile(path) as f:
         for row in f.rows:
             # Create a dict with the names and values for insertion convertion
-            row_dict = {}
-            for idx, value in enumerate(row):
-                row_dict[f.header[idx]] = value_reader(value.lstrip().rstrip())
-            yield insertion_func(table_name, row_dict)
-  
+            yield insertion_func(table_name, __row_to_header_row_dict(f.header, row))
+
+
+def csv_as_updates(path: str | Path, table_name: str, update_func: Callable = update):
+    """
+    A generator yielding the rows of a given csv file as updatet statements.
+    :param path: The path of the csv file to target.
+    :param table_name: The name of the table in the update statement.
+    :update_func: The name of the function used to construct the update statement (varying by dialect).
+    """
+    with CSVFile(path) as f:
+        for row in f.rows:
+            # Create a dict with the column names and update values.
+            yield update_func(table_name, __row_to_header_row_dict(f.header, row))
+
 
 # def csv_to_updates(path: str | Path, table_name: str, update_func: Callable = update):
 test_path = "C:/Users/AlistairKellaway/Downloads/snakes_count_100.csv"
 
 print(list(csv_as_inserts(test_path, "table_name", insertion_func=insertion)))
+print(up := list(csv_as_updates(test_path, "table_name", update_func=update)))
+for u in up:
+    print(u)
