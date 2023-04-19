a092616608b7bc6c8d7ca894988b22a3afb51648
Ali Kellaway <alistiar.kellaway@deltacapita.com>
2023-04-19 14:56:30 +0100

    Adding to the setup file.

--- setup.py
+++ setup.py
@@ -3,22 +3,18 @@ from setuptools import setup
 setup(
     name='sqlstrings',
     version='0.1.0',    
-    description='A example Python package',
+    description='Generates SQL query strings in multiple dialects.',
+    keywords='sql strings generate query',
     url='https://github.com/alikellaway/sqlstrings',
     author='Ali Kellaway',
     author_email='ali.kellaway139@gmail.com',
     license='MIT',
+    package_dir={'src':'sqlstrings'},
     packages=['sqlstrings'],
-
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: BSD License',  
-        'Operating System :: POSIX :: Linux',        
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-    ],
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',         
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent'
+    ]
 )
