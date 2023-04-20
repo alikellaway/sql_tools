825625c4d62976872644df6237f78c385c43ce58
Ali Kellaway <alistiar.kellaway@deltacapita.com>
2023-04-20 12:27:15 +0100

    Formatting setup.py.

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from pathlib import Path
 
 setup(
     name='sqlstrings',
-    version='0.1.3',    
+    version='0.1.3',
     description='Generates SQL query strings in multiple dialects.',
-    long_description= (Path(__file__).parent / "README.md").read_text(),
-    long_description_content_type = 'text/markdown',
+    long_description=(Path(__file__).parent / "README.md").read_text(),
+    long_description_content_type='text/markdown',
     keywords='sql strings generate query',
     url='https://github.com/alikellaway/sqlstrings',
     author='Ali Kellaway',
@@ -16,7 +16,7 @@ setup(
     packages=['sqlstrings'],
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',         
+        'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.8',
         'Operating System :: OS Independent'
     ]
