eb9329f85ce8ed1e71f0dfa8ad7bf00bb46a3888
Ali Kellaway <alistiar.kellaway@deltacapita.com>
2023-04-20 09:27:20 +0100

    removed redundant import.

--- sqlstrings/transact.py
+++ sqlstrings/transact.py
@@ -1,6 +1,6 @@
 from typing import Any
 import logging
-from value_handling import read_val, write_val
+from value_handling import write_val
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
