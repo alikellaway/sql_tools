da5c8a4e9d56833eeed6e50cf4423555748ca053
Ali Kellaway <alistiar.kellaway@deltacapita.com>
2023-04-20 12:26:52 +0100

    1.3 v#

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 setup(
     name='sqlstrings',
-    version='0.1.2',    
+    version='0.1.3',    
     description='Generates SQL query strings in multiple dialects.',
     long_description= (Path(__file__).parent / "README.md").read_text(),
     long_description_content_type = 'text/markdown',
