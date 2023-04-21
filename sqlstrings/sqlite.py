9cf587a140ca5291b9f13abcaeb214c04712b6be
Ali Kellaway <alistiar.kellaway@deltacapita.com>
2023-04-21 09:12:33 +0100

    Added insert into sqlite.

---
+++ sqlstrings/sqlite.py
@@ -0,0 +1,18 @@
+from typing import Any
+from sqlstrings.value_handling import write_val
+
+
+def insert(table_name: str, name_value: dict[str:Any]) -> str:
+    """
+    Generates a string that can be used to insert values into a sqlite database.
+    :param table_name: The name of the table to insert into.
+    :param name_value: A dict mapping parameter names to their respective values.
+    :return: A insert statement string that can be used to insert data insto a sqlite db.
+    """
+    outstr = f"INSERT INTO {table_name} ({', '.join(name_value.keys())})"
+    outstr += f"\nVALUES ({', '.join(map(write_val, name_value.values()))})"
+    return outstr + ";"
+
+
+if __name__ == '__main__':
+    print(insert("table_name", {'param1':200, 'param2': 20.8}))
