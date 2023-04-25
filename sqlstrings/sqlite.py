c4d20d8a02d1695e6932e7ad8a03d58900bd76f7
Ali Kellaway <alistiar.kellaway@deltacapita.com>
2023-04-25 11:08:06 +0100

    Adding inserts.

--- sqlstrings/sqlite.py
+++ sqlstrings/sqlite.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Iterable
 from sqlstrings.value_handling import write_val
 
 
@@ -14,11 +14,27 @@ def insert(table_name: str, name_value: dict[str:Any]) -> str:
     return outstr + ";"
 
 
+def inserts(table_name: str, name_values: Iterable[dict[str:Any]]) -> str:
+    """
+    Generates a string that can be used to insert multiple rows into a sqlite database.
+    :param table_name: The name of the table to insert into.
+    :param name_values: An iterable containing dictionaries mapping the names of each column 
+    to the value to fill it with.
+    :return: A string representation that can be used to insert the data.
+    """
+    outstr = f"INSERT INTO {table_name} ({', '.join(name_values[0].keys())})"
+    outstr += f"\nVALUES\n\t"
+    outstr += ",\n\t".join(
+        map(lambda d: f'({", ".join(map(write_val, d.values()))})', name_values))
+    return outstr + ";"
+
+
 def update(table: str, set: dict[str:Any], where: str = None, order: str = None, limit: int = None, offset: int = None):
     outstr = f'UPDATE {table}'
     # set
     outstr += f'\nSET '
-    outstr += "\n    ".join(map(lambda kvp: f'{kvp[0]} = {write_val(kvp[1])}', set.items()))
+    outstr += "\n    ".join(
+        map(lambda kvp: f'{kvp[0]} = {write_val(kvp[1])}', set.items()))
     # where
     outstr += "" if where is None else f"\nWHERE {where}"
     return outstr + ";"
@@ -26,4 +42,11 @@ def update(table: str, set: dict[str:Any], where: str = None, order: str = None,
 
 if __name__ == '__main__':
     # print(insert("table_name", {'param1':200, 'param2': 20.8}))
-    print(update("table", {"col1": "val1", "col2": "Val2"}, where="some conditional"))
+    # print(update("table", {"col1": "val1", "col2": "Val2"}, where="some conditional"))
+    d = {
+        'k1': 'v1',
+        'k2': 'v2',
+        'k3': 'v3',
+        'k4': 'v4'
+    }
+    print(inserts("name", [d, d, d]))
